b5602498fe8c762da67df50bdbe9949ac5cb1d6e
Winfield Ly <wly5@myseneca.ca>
2018-09-23 22:41:03 -0400

    Update TwoFer.pm

--- TwoFer.pm
+++ TwoFer.pm
@@ -13,11 +13,10 @@ our @EXPORT_OK = qw(two_fer);
 
 sub two_fer {
   my ($name) = @_;
-  if ($name) {
-    return "One for " . $name . ", one for me.";
-  } else {
-    return "One for you, one for me.";
+  if (!$name) {
+    $name = "you";
   }
+  return "One for $name, one for me.";
 }
 
 1;
